e7020f0dff1cc1ec063331709397df0fbe9684be
Savio Santos <saviogta1@gmail.com>
2021-06-05 09:57:23 -0300

    feat: creating about section

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { PublicRoutingModule } from './public-routing.module';
 import { PublicComponent } from './public.component';
 import { ComponentsModule } from './components/components.module';
+import { AboutComponent } from './sections/about/about.component';
 
 
 @NgModule({
   declarations: [
-    PublicComponent
+    PublicComponent,
+    AboutComponent
   ],
   imports: [
     CommonModule,
